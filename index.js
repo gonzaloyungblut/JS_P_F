551e8b1dc2750dcbf645250d8c29911bf7996d91
Gonzalo Yungblut <gonzaloyungblut@gmail.com>
2023-05-30 04:05:02 +0200

    iniciando simulador

---
+++ index.js
@@ -0,0 +1,15 @@
+function welcome () {
+    nombre = prompt("Bienvenido a la tienda Oficial de Racing... Por favor ingrese su nombre:")
+}
+
+
+
+welcome();
+
+let opcion = prompt (" Hola " + nombre + " ¿estas interesado en indumentaria de adultos o niños?... Ingrese 1 para adultos o 2 para niños ");
+
+console.log(nombre);
+
+console.log(opcion);
+
+
